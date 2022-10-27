7c23440b6b70f953cb0bb2caa4075d447fbdc3bf
Elpo Larisu <elpolarisu@gmail.com>
2022-10-27 13:16:23 -0300

    chore!: add departments dict

---
+++ scripts/scrapers/utilities/departments.py
@@ -0,0 +1,68 @@
+
+# A2IC
+AA = '12060003'
+# Astronomía
+AS = '3'
+# Computación
+CC = '5'
+# Civil
+CI = '6'
+# Ciencias de los Materiales
+CM = '306'
+# Deportes y Recreativos
+DR = '7'
+# Doctorado Eléctrica
+ED = '305'
+# Humanidades
+EH = '8'
+# Idiomas
+EI = '9'
+# Ingeniería e Innovación
+CD = '12060002'
+# Eléctrica
+EL = '10'
+# Escuela
+ES = '12'
+# Física
+FI = '13'
+# Geofísica
+GF = '15'
+# Geología
+GL = '16'
+# Industrial
+IN = '19'
+# Matemática
+MA = '21'
+# Mecánica
+ME = '22'
+# Minas
+MI = '23'
+# Doctorado en Materiales
+MT = '24'
+# Química y Biotecnología
+BTIQ = '307'
+
+# Departments ID
+DEPARTMENTS = {
+    'AA' : AA,
+    'DAS' : AS,
+    'DCC' : CC,
+    'DIC' : CI,
+    'CM' : CM,
+    'DR' : DR,
+    'ED' : ED,
+    'EH' : EH,
+    'EI' : EI,
+    'CD' : CD,
+    'DIE' : EL,
+    'ES' : ES,
+    'DFI' : FI,
+    'DGF' : GF,
+    'GEO' : GL,
+    'DII' : IN,
+    'DIM' : MA,
+    'DIMEC' : ME,
+    'DIMin' : MI,
+    'MT' : MT,
+    'DIQBM' : BTIQ
+}
